eed41c62ce415bf5e0a20978a3578c5797018c6f
Mark Mengelt <mmengelt@berkeley.edu>
2023-04-14 14:21:18 -0400

    added api endpoints for getting the feed, doing posts, etc

--- routes/record.js
+++ routes/record.js
@@ -22,25 +22,151 @@ const sign = (payload, privateKey, header) => {
   return `${encodedHeader}.${encodedPayload}.${signature}`;
 };
 
+
+const decode = (token) => {
+  const [encodedHeader, encodedPayload, signature] = token.split('.');
+  const header = JSON.parse(atob(encodedHeader));
+  const payload = JSON.parse(atob(encodedPayload));
+  const now = new Date();
+
+  if (now < header.expiresIn) {
+    throw new Error('Expired token');
+  }
+  return payload;
+};
+
 // recordRoutes is an instance of the express router.
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /listings.
 const apiRoutes = express.Router();
 
-// This will help us connect to the database
-//const dbo = require("../db/conn");
 
-apiRoutes.route("/test").get(async function (req, res) {
 
-    const items = [
-      { id: 1, name: 'Item 1' },
-      { id: 2, name: 'Item 2' },
-      { id: 3, name: 'Item 3' }
-    ];
+apiRoutes.route("/feed").get(async function (req, res) {
+
+  // get email from jwt
+  let bearerToken = '';
+  let decodedToken = null;
+  const bearerHeader = req.headers['authorization'];
+  console.info('bearerHeader', bearerHeader);
+  res.status(200)
+  if (typeof bearerHeader !== 'undefined') {
+    bearerToken = bearerHeader.split(' ')[1];
+    decodedToken = decode(bearerToken);
+    // TODO: Use the bearer token to authenticate the user
+  } else {
+    // Return a 401 Unauthorized response if the bearer token is missing
+    res.status(401).json({ error: 'bearer token not found in /feed request' });
+  }
   
-    res.status(200).json(items);
+  
+  // exchange user token for the email
+  let email = {email: decodedToken.email};
+  let userToken = "";
+  await axios.post('http://localhost:5001/exchange', email)
+  .then(async (tokenResponse) => {
+    userToken = tokenResponse.data.token;
+  });  
+  
+  
+  
+  // get friends of that token
+  let token = {token: userToken};
+  let friends = [];
+  await axios.post('http://localhost:5001/friends', token)
+    .then(async (tokenResponse) => {
 
-});
+      if ( tokenResponse.status === 200 ) {
+        
+        if ( 'friends' in tokenResponse.data && tokenResponse.data.friends.length > 0 ) {
+          console.info('added friends')
+          friends = tokenResponse.data.friends;
+        } else {
+          console.info('no added friends1')
+        }
+      } else {
+        console.info('no added friends2')
+      }
+      
+      console.info(friends);
+    }); 
+
+  friends.push(userToken);
+
+  const db = req.app.locals.db;
+  const collection = db.collection('posts'); // Replace 'your_collection_name' with the actual collection name.
+
+  // Query the collection for documents where the field value is in the keySet
+  const query = { token: { $in: friends } };
+  const documents = await collection.find(query).toArray();
+  console.info(`found ${documents.length} posts from network, searched`, friends)
+
+  let sortedDocuments = documents.map(doc => {
+    return {...doc, sortableDate: new Date(doc.dateAdded).getTime() }
+  })
+
+  sortedDocuments = sortedDocuments.sort( (a,b) => b.sortableDate - a.sortableDate)
+
+  res.status(200).json(sortedDocuments);
+  /*
+  // create a set of tokens
+
+  // get all posts for all tokens in the set
+
+  // order posts by most recent
+
+  // return results
+  res.status(200).json({ friends });
+  */
+})
+
+apiRoutes.route("/post").post(async function (req, res) {
+
+  const post = req.body.post;
+
+  console.info('/n * user posting')
+  let bearerToken = '';
+  let decodedToken = null;
+  const bearerHeader = req.headers['authorization'];
+  if (typeof bearerHeader !== 'undefined') {
+    bearerToken = bearerHeader.split(' ')[1];
+    decodedToken = decode(bearerToken);
+    // TODO: Use the bearer token to authenticate the user
+  } else {
+    // Return a 401 Unauthorized response if the bearer token is missing
+    
+    res.sendStatus(401);
+  }
+
+  // decode email from jwt, get token from token server, insert into posts collection using token
+
+  console.info('decodedToken', decodedToken)
+
+  let email = {email: decodedToken.email}
+
+  axios.post('http://localhost:5001/exchange', email)
+    .then(async (tokenResponse) => {
+      console.info(tokenResponse.data)
+
+      let userToken = tokenResponse.data.token;
+      // make the entry into the posts collection
+
+      const db = req.app.locals.db;
+      const postsCollection = db.collection('posts');
+
+      const result = await postsCollection.insertOne({ token: userToken, post, dateAdded: new Date() });
+
+      res.sendStatus(200)
+    })
+    .catch(e => {
+      console.info('error', e)
+      res.sendStatus(500)
+    });
+
+
+
+  
+})
 
 apiRoutes.route("/login").post(async function (req, res) {
 
